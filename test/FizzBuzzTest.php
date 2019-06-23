d989bceafd95b9ba109f091e86564bd090002cf6
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2019-06-23 16:05:51 +0930

    Update FizzBuzzTest.php

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -2,56 +2,44 @@
 
 require "src/./FizzBuzz.php";
 
-class FizzBuzzTest extends PHPUnit_Framework_TestCase {
-  
-private $fizzbuzz;
+class FizzBuzz extends PHPUnit_Framework_TestCase {
+
+  private $fizzbuzz;
 
   function __construct() {
-     $this->fizzbuzz = new Fizzbuzz();
+    
+    $this->fizzbuzz = new Fizzbuzz();
   }
   
   function testResultShouldBeOne()
     
   {
-     $result = $this->fizzbuzz->execute(1);
-    
-     $this->assertEquals($result, '1');
-   }
+    $result = $this->fizzbuzz->execute(1);
+    $this->assertEquals($result, '1');
+  }
   
   function testResultShouldBeTwo()
     
   {
-      $result = $this->fizzbuzz->execute(2);
+    $result = $this->fizzbuzz->execute(2);
+    $this->assertEquals($result, '2');
     
-      $this->assertEquals($result, '2');
-   }
-  
-   function testResultShouldBeFizz()
-     
-   {
-      $result = $this->fizzbuzz->execute(3);
-     
-      $this->assertEquals($result, 'fizz');
-   }
-  
-    function testResultShouldBeBuzz()
-      
-   {
-      $result = $this->fizzbuzz->execute(4);
-      
-      $this->assertEquals($result, 'buzz');
-      
-   }
-  
-     function testResultShouldDisplayResults()
-       
-    {
-      $result = $this->fizzbuzz->execute(5);
-       
-      $this->assertEquals($result, '1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 buzz');
-       
-    }
-  
+  }
+  function testResultShouldBeFizz()
+  {
+    
+    $result = $this->fizzbuzz->execute(3);
+    $this->assertEquals($result, 'fizz');
+    
+  }
+  function testResultShouldBeBuzz()
+  {
+    
+    $result = $this->fizzbuzz->execute(4);
+    $this->assertEquals($result, 'buzz');
+    
   }
   
+}
+
  ?>
