8bbb5ce1c076bdae0cfbc59475388084a19ee278
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2019-06-23 14:17:32 +0930

    Update FizzBuzz.php

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -2,23 +2,23 @@
 
 class Fizzbuzz
 {
-			public function execute($number)
-			{
-						if( ($number % 3) == 0 ) return 'fizz';
-						if( ($number % 5) == 0 ) return 'buzz';
-
-						return $number;
-			}
-			public function play($end = 100)
-			{
-							$start = 1;
-							$result = '';
-							for( $number = $start; $number <= $end; $number ++ )
-							{
-							$result .= $this->execute($number) . ' ';
-							}
-							return substr($result, 0, -1);
-				}
+	public function execute($number)
+	{	
+		if( ($number % 3) == 0) return 'fizz';
+		if( ($number % 5) == 0) return 'buzz';
+	
+		return $number;
 	}
+	public function play($end = 100)
+	{
+		$start = 1;
+		$result = '';
+		for( $number = $start; $number <= $end; $number ++ )
+		{
+		$result .= $this->execute($number) . '';
+		}
+		return substr($result, 0, -1);
+	}
+}
 
 ?>
