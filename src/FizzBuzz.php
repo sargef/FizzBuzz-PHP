4004e1ba05ba7140091c3d1b35021e72aecd62c9
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2019-06-24 06:59:10 +0930

    Update FizzBuzz.php

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -4,15 +4,19 @@ class Fizzbuzz
 {
 	public function execute($number)
 	{
+		//if multiplied by 3, return fizz
 		if( ($number % 3) == 0 ) return 'fizz';
+		//if multiplied by 5, return buzz
 		if( ($number % 5) == 0 ) return 'buzz';
 
 		return $number;
 	}
 	public function play($end = 100)
 	{
+		//begin at number 1
 		$start = 1;
 		$result = '';
+		//for every number from 1 through to 100, add 1 and return that result
 		for( $number = $start; $number <= $end; $number ++ )
 		{
 		$result .= $this->execute($number) . '';
